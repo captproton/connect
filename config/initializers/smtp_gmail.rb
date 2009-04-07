359fad8dbc16c6de8f22b4ba8af54bad708fb48a
Carl Tanner <captproton@betterbilling.net>
2009-04-06 23:53:43 -0700

    adjusting..

--- config/initializers/smtp_gmail.rb
+++
@@ -1,8 +0,0 @@
-require "smtp_tls" 
-ActionMailer::Base.smtp_settings = { 
-:address => "smtp.gmail.com", 
-:port => 587, 
-:user_name => "paloozahub@gmail.com", 
-:password => "Heawen2wor", 
-:authentication => :plain 
-}
